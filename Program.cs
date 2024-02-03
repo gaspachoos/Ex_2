7e80cf70ac3d533fe5a9131b1dd611a9710bc1aa
Garik <gar.minasyan.97@mail.ru>
2024-02-03 10:50:44 +0400

    Добавили функцию Аккермана

--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿int Ackermann(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (m > 0 && n == 0)
+    {
+        return Ackermann(m - 1, 1);
+    }
+    else if (m > 0 && n > 0)
+    {
+        return Ackermann(m - 1, Ackermann(m, n - 1));
+    }
+
+    return 0;
+}
